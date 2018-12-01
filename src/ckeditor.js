dc889baa1d78de2cb17b2ea4a03e644419969ee3
vdfor <jiakun.li@qq.com>
2018-12-01 16:25:24 +0800

    custom build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -32,6 +32,7 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import TextColor from 'ckeditor-text-color-select/src/textcolor';
 
 export default class ClassicEditor extends ClassicEditorBase { }
@@ -63,7 +64,8 @@ ClassicEditor.builtinPlugins = [
 	Underline,
 	Strikethrough,
 	FontSize,
-	TextColor
+	TextColor,
+	Alignment
 ];
 
 // Editor configuration.
@@ -81,6 +83,7 @@ ClassicEditor.defaultConfig = {
 			'italic',
 			'underline',
 			'strikethrough',
+			'alignment',
 			'link',
 			'bulletedList',
 			'numberedList',
